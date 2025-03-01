9a3f3d1fb7216051498587fafdfcb917176dd1be
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-03-01 10:47:43 -0500

    feat: add `svelte-preprocess`, `postcss`, and `postcss-load-config` packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [vitePreprocess(), mdsvex()],
+  preprocess: [vitePreprocess(), mdsvex(), sveltePreprocess()],
   kit: {
     adapter: adapter({
       fallback: 'not_found.html',
