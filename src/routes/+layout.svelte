570309935fc905e268b6f3d3d056d1c663925d07
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-03-01 10:28:54 -0500

    feat: add and use `@fontsource/inter` and `@fontsource/ibm-plex-mono` packages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import '../app.css';
 
+  import '@fontsource/inter/400.css';
+  import '@fontsource/inter/400-italic.css';
+  import '@fontsource/ibm-plex-mono/700.css';
+  import '@fontsource/ibm-plex-mono/700-italic.css';
+
   import Footer from '$lib/components/Footer.svelte';
   import Header from '$lib/components/Header.svelte';
   import Sidebar from '$lib/components/Sidebar.svelte';
